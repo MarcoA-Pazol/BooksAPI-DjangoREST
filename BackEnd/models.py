7335e4d84dba16496f5211fa42a1cd814136aec6
Marco Pazol <donjulionlimon@gmail.com>
2024-07-29 21:28:16 -0600

    Create DB with PostgreSQL[Done]. Create Author and Book models in ./BackEnd/models.py[Done]. Makemigrations and migrate DB models[Done]

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    country = models.CharField(max_length=100, null=False)
+    
+    name = first_name, last_name
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    author = models.ForeignKey(Author, related_name="author", on_delete=models.CASCADE)
+    genre = models.CharField(max_length=50, null=False),
+    synopsis = models.TextField()
+    
+    def __str__(self):
+        return self.title
--- BooksAPI/settings.py
+++ BooksAPI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BackEnd',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +74,14 @@ WSGI_APPLICATION = 'BooksAPI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BooksApi',
+        'HOST': '127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': 'mach1029',
+        'PORT': '5432'
     }
 }
 
