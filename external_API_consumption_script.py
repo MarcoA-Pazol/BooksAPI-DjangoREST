e289466aa18f81005c38739042c69b1c8393a07c
Marco Pazol <donjulionlimon@gmail.com>
2024-08-05 20:25:48 -0600

    Implement an external script to test the API GET endpoints work as expected[Done].

---
+++ external_API_consumption_script.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+
+# Define the URL of the API to get the data from.
+authors_url = "http://localhost:8600/api/author/"
+books_url = "http://localhost:8600/api/book/"
+
+# [Optional] If your API endpoint requires authentication, include your token here.
+api_token = "your-api-token-here"
+headers = {
+    "Authorization":api_token,
+}
+
+# Send GET request to the API endpoint
+response = requests.get(authors_url, headers=headers)
+
+# Return response code from succesfully request.
+if response.status_code == 200:
+    # Get the data as a json format.
+    data = response.json()
+    
+    # Transform json to a Data Frame from pandas to show it as a table on terminal.
+    data_frame = pd.DataFrame(data)
+    
+    print("Data: ", data_frame)
+else:
+    print("Request has failed:", response.status_code)
